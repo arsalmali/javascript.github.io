7fb0dda735942ac476c093a1e24fc85bb5be25e1
arsalmali <134054275+arsalmali@users.noreply.github.com>
2023-05-19 14:47:37 +0500

    Add files via upload
    ok

---
+++ app.js
@@ -0,0 +1,485 @@
+//  Chapter 1 (Alerts)
+
+
+
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone Number");
+// alert("Password");
+
+// alert("You're learning JavaScript!");
+
+//  console.log(alert);
+
+
+
+
+
+
+//  Chapter 2 (Variables for string)
+
+
+
+// 1
+
+
+// var myVariablesName;
+
+// // 2
+
+
+// var myFullName;
+// myFullName = "Arsal Malik!";
+
+// console.log(myFullName);
+
+// // 3
+
+// var teamName = "Team Name >  Coding Tigers";
+// alert(teamName);
+
+// // 4
+
+
+
+// var bestMan = "Charlie";
+
+// bestMan = "john";
+
+
+
+
+
+//           Chapter 3 (Variables for numbers)
+
+
+
+//  1
+
+// var caseQty;
+
+
+
+
+// //  2
+
+
+// caseQty = 144;
+
+
+
+// //  3
+
+
+// var num = "9";
+// num = parseInt(num);
+
+
+// //  4
+
+
+
+// var result;
+// result = 5 + 4;
+
+
+// //  5
+
+
+// var merchtotal = 100;
+// var shippingcharge = 10;
+// var orderTotal = merchtotal + shippingcharge;
+// console.log(orderTotal);
+
+// //   6
+
+// var number = 10;
+// number = number + 3;
+
+// console.log(number);
+
+
+
+//  Chapter 4 (Variable names Legal and Illegal)
+
+// 1
+
+
+// var productcost = 3.45;
+
+
+// // 2
+
+
+// var nameOfBrand;
+
+
+// // 3
+
+// var myNumber = 42;
+
+// //  4
+
+
+// var ArsalMalik;
+
+//  5   List the legal and Illegal Variables.
+
+
+//     legal variables names
+
+//   1    var _internalVar;
+//   2    var $price;
+//   3    var user123;
+//   4    var café;
+
+
+//    illegal variable names
+
+//     1     var 123abc;
+//     2     var my-variable;
+//     3     var var;
+//     4     var Math;
+
+
+
+//     Chapter 5 (Math Expression I)
+
+//   1 & 2
+
+
+// var num = 10 % 3;
+// console.log(num)
+
+
+// //  3
+
+// var largeNum = 1000 * 2000;
+
+// console.log(largeNum);
+
+
+// //   4
+
+
+// variable1 = 10
+// variable2 = 5
+
+// let = variable1 - variable2;
+// console.log(let)
+
+
+// //  5
+
+
+// price1 = 25
+// price2 = 7
+// result = price1 % price2;
+// console.log(result);
+
+
+
+// //   6
+
+
+// var num1 = 10;
+// var num2 = 5;
+// var result = num1 * num2;
+
+// alert("The result of the multiplication is: " + result);
+
+
+
+//    Chapter 6 (Math Expression II)
+
+
+// 1
+
+
+
+// var x = 100;
+// --x;
+
+// console.log(x)
+
+
+
+// //  2     3     &    4
+
+// var x = 50;
+// var y = x++;
+
+// console.log(y);
+
+// var y = 50;
+// var z = --y;
+
+// console.log(z);
+
+
+//   5
+
+
+// var num = 10;
+// var newNum = ++num;
+
+// console.log(num)
+
+
+//   6
+
+
+// newvariable = originalvariable + 1;
+
+
+
+//    7
+
+
+// var number = 5;
+
+// number++;
+
+// alert("The New Value Is: " + number)
+
+
+//         Chapter 7 (Math Expression III)
+
+
+
+//    1
+
+
+
+// var calculatedNum = 5 + (5 * 5);
+
+// console.log(calculatedNum);
+
+
+//   2
+
+
+// var calculatedNum = (3 + 3) * 6;
+
+// console.log(calculatedNum);
+
+
+//   3
+
+
+// var calculatedNum = (3 + 3) * (4 + 2);
+
+// console.log(calculatedNum);
+
+
+//   4
+
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+
+
+
+// 5
+
+
+// var cost = (2 + 2) * (4 + 10);
+
+// console.log(cost);
+
+
+//   6
+
+
+// var cost2 = (1 + 1) * (0 + 10);
+// console.log(cost2);
+
+
+//   7
+
+// var cost3 = (2 / 2) * (5)
+
+// console.log(cost3);
+
+
+
+//    Chapter 8 (Concatenating Text Strings)
+
+
+
+//   1
+
+// var num = "2" + "2";
+
+// console.log(num);
+
+
+//   2
+
+
+// var message = ("Hello," + "Dolly");
+// alert("Hello"  +  "Dolly?");
+// console.log(message);
+
+
+//   3
+
+// alert("33" + 3);
+
+
+//  4
+
+// var firstname = "Pakistan";
+// var secondname = "Zindabad";
+// var concatenationString = firstname + " " + secondname;
+// alert(concatenationString);
+
+//  5
+
+// var myString = "The number is: ";
+// var myNumber = 42;
+
+// var result = myString + myNumber;
+
+// console.log(result);
+
+
+// //  6
+
+// var firststring = "Hello";
+// var Secondstring = "World";
+// var concatenatedString = firststring + " " + Secondstring;
+
+// console.log(concatenatedString);
+
+
+//   Chapter 9 (Prompts)
+
+
+//  1
+
+// var firstname = prompt("Enter first name");
+// console.log(firstname);
+
+
+//  2
+
+// var country = prompt("Country?", "China");
+// console.log(country);
+
+//  3
+
+
+// let yourName = prompt("Enter Your Name");
+
+
+// console.log(yourName);
+
+
+
+//   4
+
+// var userOutput = prompt("Please enter your input:", "Default input");
+// console.log(userOutput);
+
+//    5
+
+
+// var message = "Enter your name:";
+// var name = "John Doe";
+
+// var userInput = prompt(message, name);
+
+// console.log(userInput);
+
+
+
+//   6
+
+
+// var name = prompt("Please Enter Your Name", "John Due")
+// alert("You Entered: " + name)
+// console.log(name);
+
+
+//    Chapter 10 (if statments)
+
+
+//  1
+
+
+// var city = prompt("Karachi");
+// if (city === "Karachi") {
+//     console.log("The City of Lights");
+// }
+
+
+//  2
+
+// var x = 10;
+
+// var y = 10;
+
+// if (x === y) {
+//     var z = prompt("Please enter the value of z:");
+// }
+
+
+
+//   3
+
+// var ZipCode = "10010";
+
+
+// if (ZipCode === "10010") {
+//     alert("Karachi");
+// } else {
+//     alert("Please write the correct city");
+// }
+
+
+
+//   4
+
+// var x = 1;
+
+// if (x === 1) {
+//     x = 10;
+
+// }
+// console.log(x);
+
+
+
+//     Chapter 11 (Comparison Operators)
+
+
+//  1
+
+// let variable1 = 5;
+
+// let variable2 = 20;
+
+
+// if (variable1 !== variable2) {
+
+//   }
+
+//   console.log(variable1,variable2);
+
+
+//   2
+
+
+
+// let variable1 = 3;
+
+// let variable2 = 2;
+
+// if (variable1 >= variable2) {
+//     alert("True");
+// }
+
+
+//  3
